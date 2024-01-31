91a94f959233ec06784294e61b3b1a76e2d5fe51
anshul9809 <kumar.anshul9809@gmail.com>
2024-01-31 19:09:44 +0530

    Second-last-commit

--- script.js
+++ script.js
@@ -29,4 +29,23 @@ $(document).ready(function(){
       var index = $(this).index();
       $('#main-carousel').trigger('to.owl.carousel', [index, 300, true]);
     });
-  });
+
+    $('.testimonial__slides').owlCarousel({
+      loop: true,
+      margin:10,
+      nav: true,
+      dots:false,
+      items: 3,
+      responsive: {
+          0: {
+              items: 1,
+          },
+          600: {
+              items: 2,
+          },
+          1000: {
+              items: 3,
+          }
+      }
+  })
+});
